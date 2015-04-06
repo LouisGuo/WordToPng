9bf2ba9afe901895a3cf1fc57194b791db2340d6
oword <2213429531@qq.com>
2015-04-07 02:13:20 +0800

    NO Comment

--- Code/WordToPng/WordToPng/App.xaml.cs
+++ Code/WordToPng/WordToPng/App.xaml.cs
@@ -17,11 +17,9 @@ namespace WordToPng
     {
         public static List<string> fullPathList = new List<string>();
         public static string itemName = "WordToPng";
-        //public string itemPath = AppDomain.CurrentDomain.DomainManager.EntryAssembly.Location + " %1";//当前exe文件夹路径
+        public string itemPath = AppDomain.CurrentDomain.DomainManager.EntryAssembly.Location + " %1";//当前exe文件夹路径
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            
-
             if(e.Args.Length>0)
             {
                 for (int i = 0; i < e.Args.Length; i++)
@@ -34,6 +32,11 @@ namespace WordToPng
                 //RegistryKeyHelper.DeleteContextMenu(itemName);
                 //RegistryKeyHelper.AddFileContextMenuItem(itemName, itemPath);
             }
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message);
         }
     }
 }
